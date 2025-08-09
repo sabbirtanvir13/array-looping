a734be6d21f6b13a0bc53a37acbcdd697a365f97
Tanvir <sabbirtanvirahmed18@gmail.com>
2025-08-09 12:26:19 +0600

    first commit

---
+++ problem1.js
@@ -0,0 +1,9 @@
+const names=['ahona', 'sabbir','rupali','mohona','tanvir']
+const  reversed=[]
+ for(let i =0; i<names.length;i++){
+    console.log(names[i]);
+    reversed.unshift(names[i])
+
+ }
+ console.log( reversed)
+ 
---
+++ problem3.js
@@ -0,0 +1,6 @@
+var numbers = ['Tom', 'Tim', 'Tin', 'Tik']
+var result = '';
+for(var names of numbers){
+    result+=names
+}
+console.log(result)
