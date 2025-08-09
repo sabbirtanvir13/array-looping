e3a708101af7a21a6e494f5828da619400ac5c9a
Tanvir <sabbirtanvirahmed18@gmail.com>
2025-08-09 13:39:01 +0600

    added students name or marks

---
+++ problem5.js
@@ -0,0 +1,8 @@
+
+const students=[{},{}];
+const result=[
+ {name:'sabbir',marks:90},
+ {name:'tanvir',marks:80},
+]
+console.log(result[0].name,'scored',result[0].marks);
+console.log(result[1].name,'scored',result[1].marks);
